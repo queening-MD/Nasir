d76216d5f38bc84545a4fc834e171ebd211779c5
queening-MD <abbaskaleem244244@gmail.com>
2025-08-15 08:29:21 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "CZRVFCwZ#F_g-EnArV_mTlkCunXLL-dxeryQSSwK47PRtWndXFYI", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "iVghhJyC#Az5vBdrXGmq5iFsN0in0bkTZ2nmgKkxDhArJ2SSibms", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 MODE: process.env.MODE || "public",  //public | private | inbox | group
 OWNER_NUMBER: process.env.OWNER_NUMBER || "923248259064",
